c3d5a06597ebf3d14f3493fb5701ffe8599df1a3
nepahka <franke.hatchery@gmail.com>
2018-08-03 12:52:08 +0300

    test

--- config/index.js
+++ config/index.js
@@ -49,9 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    // assetsSubDirectory: 'static',
-    assetsSubDirectory: '',
-    assetsPublicPath: './',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
